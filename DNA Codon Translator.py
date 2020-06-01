07bb0fa37516c8717acfd7b55eb693bc4006bf19
Jawahar <Jtunuguntla@gmail.com>
2020-06-01 14:46:29 +0100

    Commit updated script docstrings

--- DNA Codon Translator.py
+++ DNA Codon Translator.py
@@ -4,15 +4,38 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from Bio.SeqUtils import seq3
 
+
 #Updates user entered sequence in the GUI
 def nucleotide_entered(nucleotide):
+    '''The DNA base (nucleotide) entered by user is added to new sequence or adds to
+    existing sequence.
+
+    Parameters
+    __________
+    nucleotide: str, not optional
+        The user entered DNA base.
+
+    Return
+    ______
+    updated sequence to user's UI.'''
+    
     if sequence_text.get() != "Enter Sequence":
         sequence_text.set(sequence_text.get() + nucleotide)
     else:
         sequence_text.set(nucleotide)
 
+
 #Converts the DNA sequence entered by the user
 def get_protein():
+    ''' Translates DNA sequence entered by user.
+    Parameters
+    __________
+    No parameters only global variable sequence_text required.
+
+    Return
+    ______
+    Separate showinfo box with converted amino acid sequence.'''
+
     str_sequence_text = str(sequence_text.get())
     if (((len(str_sequence_text)) % 3) == 0) and (len(str_sequence_text) != 0):
         dna = Seq(str_sequence_text, IUPAC.unambiguous_dna)
@@ -28,12 +51,24 @@ def get_protein():
     else:
         messagebox.showerror("!", "Sequence needs to be a multiple of 3 or sequence should not be 0.")
 
+
 #Deletes the latest nucletide entered in the sequence
 def backspace():
+    '''Removes the last entered DNA base in sequence.
+
+    Parameters
+    __________
+    No sepecifc parameter other than global variable sequence_text.
+
+    Returns
+    ______
+    new sequence with only last entered DNA base removed.'''
+    
     return sequence_text.set(str(sequence_text.get())[:-1])
 
+
 #The main GUI control centre
-def main():
+def main():        
     global sequence_text
     
     root = tk.Tk()
